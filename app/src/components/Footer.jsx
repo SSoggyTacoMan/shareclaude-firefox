cf11e71d132facfb233df92650656e0fb57275f0
SSoggyTacoMan <maarenaurich@gmail.com>
2026-03-12 17:30:43 +0100

    Reduce vertical spacing in Footer component for improved layout

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer className="py-4 border-t border-gray-700">
-            <div className="flex flex-col items-center justify-between px-4 mx-auto space-y-4 max-w-7xl sm:flex-row sm:space-y-0">
+            <div className="flex flex-col items-center justify-between px-4 mx-auto space-y-2 max-w-7xl sm:flex-row sm:space-y-0">
                 <div className="flex space-x-6">
                     <a href="https://chromewebstore.google.com/detail/pcpjdbnjhgofgjgegodlnebdnmiddmaa" target="_blank" rel="noopener noreferrer">
                         <Chrome className="w-5 h-5 text-gray-400 hover:text-gray-200" />
