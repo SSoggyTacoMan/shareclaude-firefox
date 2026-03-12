10aed3aae8500fa85f817e3e730b115c216eea2b
SSoggyTacoMan <maarenaurich@gmail.com>
2026-03-12 12:36:13 +0100

    Remove unused loader SVG from content.js

--- extension/content.js
+++ extension/content.js
@@ -2,8 +2,6 @@ const PAGE_URL = 'https://shareclaude.pages.dev'
 const CLAUDE_API_URL = 'https://claude.ai/api/organizations'
 let organizationId = ''
 
-const loaderSVG = `<svg width="20px" height="20px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" class="animate-spin text-white"><g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"><path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8z" opacity=".2"/><path d="M7.25.75A.75.75 0 018 0a8 8 0 018 8 .75.75 0 01-1.5 0A6.5 6.5 0 008 1.5a.75.75 0 01-.75-.75z"/></g></svg>`
-
 // Store artifacts content globally for updates
 const artifactsCache = new Map()
 
